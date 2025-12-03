72c190c5c09ef7fa7a31e3255faed9a4a2861f5f
lgm3992 <jkl0133@naver.com>
2025-12-03 13:36:13 +0900

    3rd Commit

--- build_dll.bat
+++ build_dll.bat
@@ -1,18 +1,26 @@
 @echo off
 chcp 65001 >nul
-g++ main.cpp glad_4.6/src/glad.c -mconsole ^
--I"glfw_3.4/include" ^
--I"glad_4.6/include" ^
--I"glm_0.9.9.8" ^
--L"glfw_3.4/lib-mingw-w64" ^
--lglfw3dll -lopengl32 -lgdi32 -luser32 -lshell32 ^
--o main.exe
+echo [진행] DLL 빌드...
 
-if %errorlevel% == 0 (
-    copy "glfw_3.4\lib-mingw-w64\glfw3.dll" . >nul
-    
-    main.exe
+g++ main.cpp glad_4.6/src/glad.c -mconsole -Iglfw_3.4/include -Iglad_4.6/include -Iglm_0.9.9.8 -Lglfw_3.4/lib-mingw-w64 -lglfw3 -lopengl32 -lgdi32 -luser32 -lshell32 -shared -o main.dll
+
+if %errorlevel%==0 (
+    copy glfw_3.4\lib-mingw-w64\glfw3.dll . >nul
+    echo [성공] DLL+glfw3.dll 준비됨
 ) else (
-    echo [실패] 빌드 에러!
+    echo [실패] DLL 빌드 실패!
+    pause
+    exit /b 1
 )
+
+echo [진행] 실행파일 빌드...
+g++ main.cpp glad_4.6/src/glad.c -mconsole -Iglfw_3.4/include -Iglad_4.6/include -Iglm_0.9.9.8 -L. -Lglfw_3.4/lib-mingw-w64 -lmain -lglfw3 -lopengl32 -lgdi32 -luser32 -lshell32 -o main_test.exe
+
+if %errorlevel%==0 (
+    echo [완료] OpenGL 창 실행!
+    main_test.exe
+) else (
+    echo [실패] 실행파일 빌드 실패!
+)
+
 pause
