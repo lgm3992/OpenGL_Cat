0b7a751ba9834057237c832c90b0ccb35fc6f7fa
lgm3992 <jkl0133@naver.com>
2025-12-03 17:37:58 +0900

    Fix encoding issue

--- build_dll.bat
+++ build_dll.bat
@@ -1,26 +1,26 @@
 @echo off
 chcp 65001 >nul
-echo [진행] DLL 빌드...
+echo [Process] Build DLL...
 
 g++ main.cpp glad_4.6/src/glad.c -mconsole -Iglfw_3.4/include -Iglad_4.6/include -Iglm_0.9.9.8 -Lglfw_3.4/lib-mingw-w64 -lglfw3 -lopengl32 -lgdi32 -luser32 -lshell32 -shared -o main.dll
 
 if %errorlevel%==0 (
     copy glfw_3.4\lib-mingw-w64\glfw3.dll . >nul
-    echo [성공] DLL+glfw3.dll 준비됨
+    echo [Success] DLL+glfw3.dll Ready.
 ) else (
-    echo [실패] DLL 빌드 실패!
+    echo [Failure] Failure to Build DLL!
     pause
     exit /b 1
 )
 
-echo [진행] 실행파일 빌드...
+echo [Process] Build exe File...
 g++ main.cpp glad_4.6/src/glad.c -mconsole -Iglfw_3.4/include -Iglad_4.6/include -Iglm_0.9.9.8 -L. -Lglfw_3.4/lib-mingw-w64 -lmain -lglfw3 -lopengl32 -lgdi32 -luser32 -lshell32 -o main_test.exe
 
 if %errorlevel%==0 (
-    echo [완료] OpenGL 창 실행!
+    echo [Success] Execute OpenGL Window!
     main_test.exe
 ) else (
-    echo [실패] 실행파일 빌드 실패!
+    echo [Failure] Failure to build exe File!
 )
 
 pause
